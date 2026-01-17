bd0d3becb819ef0858e4d4c04795111dd6878268
Maneatingroblox <omgrobloxishacked@gmail.com>
2026-01-16 19:54:39 -0500

    Downloading Files Test

---
+++ static/assets/js/f1.js
@@ -0,0 +1,52 @@
+async function loadDownloads() {
+    const response = await fetch('/assets/json/f.json'); // Path to your json
+    const data = await response.json();
+    const container = document.querySelector(".apps");
+    
+    if (!container) return;
+    container.innerHTML = "";
+
+    data.forEach(item => {
+        const card = document.createElement("div");
+        card.className = "column"; 
+        card.innerHTML = `
+            <div class="container-card">
+                <div class="wrapper">
+                    <img src="${item.image}" alt="${item.name}" loading="lazy">
+                </div>
+                <div class="container-info">
+                    <p>${item.name}</p>
+                </div>
+            </div>
+        `;
+
+        // The Download Trigger
+        card.onclick = () => {
+            const link = document.createElement("a");
+            link.href = item.file;
+            link.download = item.name; // Use the name from JSON as filename
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        };
+
+        container.appendChild(card);
+    });
+}
+
+// Search Filter Logic
+function bar() {
+    let input = document.getElementById('search').value.toLowerCase();
+    let cards = document.getElementsByClassName('column');
+
+    for (let i = 0; i < cards.length; i++) {
+        let name = cards[i].querySelector('.container-info p').innerText.toLowerCase();
+        if (name.includes(input)) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", loadDownloads);
--- static/assets/js/m1.js
+++ static/assets/js/m1.js
@@ -43,7 +43,9 @@ document.addEventListener("DOMContentLoaded", () => {
         <a class="navbar-link" href="/./a"><i class="fa-solid fa-gamepad navbar-icon"></i><an>&#71;&#97;</an><an>&#109;&#101;&#115;</an></a>
         <a class="navbar-link" href="/./b"><i class="fa-solid fa-phone navbar-icon"></i><an>&#65;&#112;</an><an>&#112;&#115;</an></a>
         
-        <a class="navbar-link" href="/videos.html"><i class="fa-solid fa-video navbar-icon"></i><an>&#77;&#101;</an><an>&#100;&#105;&#97;</an></a>
+        <a class="navbar-link" href="/videos.html"><i class="fa-solid fa-video navbar-icon"></i><an>&#86;&#105;</an><an>&#100;&#101;&#111;</an></a>
+
+        <a class="navbar-link" href="/files.html"><i class="fa-solid fa-file navbar-icon"></i><an>&#77;&#101;</an><an>&#100;&#105;&#97;</an></a>
         
         ${qp ? "" : '<a class="navbar-link" href="/./d"><i class="fa-solid fa-laptop navbar-icon"></i><an>&#84;&#97;</an><an>&#98;&#115;</an></a>'}
         <a class="navbar-link" href="/./c"><i class="fa-solid fa-gear navbar-icon settings-icon"></i><an>&#83;&#101;&#116;</an><an>&#116;&#105;&#110;&#103;</an></a>
